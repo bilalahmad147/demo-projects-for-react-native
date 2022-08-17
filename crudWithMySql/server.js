a42cd39826c999e06223a04f5407fd7934bf6ab1
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2022-08-18 01:15:32 +0500

    Update server.js

---
+++ crudWithMySql/server.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var app = express();
+
+var mysql = require('mysql');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json({type: 'application/json'}));
+app.use(bodyParser.urlencoded({extended: true}));
+
+var con = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '', //empty for window
+  database: 'studentsdetails',
+});
+
+var server = app.listen(4545, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('start');
+});
+
+con.connect(function (error) {
+  if (error) console.log(error);
+  else console.log('connected');
+});
+
+app.get('/user', function (req, res) {
+  con.query('select * from users', function (error, rows, fields) {
+    if (error) console.log(error);
+    else {
+      console.log(rows);
+      res.send(rows);
+    }
+  });
+});
