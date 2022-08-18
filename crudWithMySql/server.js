f2e2ff4ff0e1a2a1996e5062f00f8553f996d4e7
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2022-08-19 00:24:07 +0500

    updated changes

--- crudWithMySql/server.js
+++ crudWithMySql/server.js
@@ -1,42 +1,32 @@
 var express = require('express');
 var app = express();
 
-var mysql = require('mysql');
+var mysql = require('mysql2');
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.json({type: 'application/json'}));
 app.use(bodyParser.urlencoded({extended: true}));
 
-var con = mysql.createConnection({
+var db = mysql.createConnection({
   host: 'localhost',
+  port: '3300',
   user: 'root',
-  password: '', //empty for window
-  database: 'studentsdetails',
+  password: 'rootpassword', //empty for window
+  database: 'crud_contact',
 });
 
-var server = app.listen(4545, function () {
+var server = app.listen(5000, function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log('start');
 });
 
-con.connect(function (error) {
-  if (error) console.log(error);
-  else console.log('connected');
-});
-
 app.get('/', function (req, res) {
   const sqlInsert =
-    "INSERT INTO users (id, name, email, degree) VALUES (6, 'zubi', 'zubi@gmail.com', 'BS chemical engineering')";
-  con.query(sqlInsert, (error, result) => {
-    console.log("error", error);
-    console.log("result", result);
-  })
-  con.query('select * from users', function (error, rows, fields) {
-    if (error) console.log(error);
-    else {
-      console.log(rows);
-      res.send(rows);
-    }
+    "INSERT INTO contact_db (name,email,contact) VALUES ('ahmad', 'ahmad@gmail.com', 00112233)";
+  db.query(sqlInsert, (error, result) => {
+    console.log('error =>', error);
+    console.log('result =>', result);
   });
+  res.send('hello express from server file');
 });
