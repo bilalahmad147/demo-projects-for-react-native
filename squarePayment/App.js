3f40bf7defa554efc06da68631bd8eab0d44d09d
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2022-08-22 17:44:44 +0500

    updated changes

--- squarePayment/App.js
+++ squarePayment/App.js
@@ -1,112 +1,64 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, {Component} from 'react';
+import {Alert, Button, ScrollView, StyleSheet, Text, View} from 'react-native';
+import {SQIPCore, SQIPCardEntry} from 'react-native-square-in-app-payments';
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+export default class App extends Component {
+  async componentDidMount() {
+    await SQIPCore.setSquareApplicationId(
+      'sandbox-sq0idb-mpqW7KEW-B2mRkERFkd4yg',
+    );
+  }
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  constructor() {
+    super();
+    this.onStartCardEntry = this.onStartCardEntry.bind(this);
+    this.onCardNonceRequestSuccess = this.onCardNonceRequestSuccess.bind(this);
+  }
+  onCardEntryComplete() {
+    Alert('Card entry completed');
+  }
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+  async onCardNonceRequestSuccess(cardDetails) {
+    try {
+      await SQIPCardEntry.completeCardEntry(this.onCardEntryComplete());
+    } catch (ex) {
+      await SQIPCardEntry.showCardNonceProcessingError(ex.message);
+    }
+  }
+  onCardEntryCancel() {
+    Alert('Card entry cancelled');
+  }
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  async onStartCardEntry() {
+    const cardEntryConfig = {
+      collectPostalCode: false,
+    };
+    await SQIPCardEntry.startCardEntryFlow(
+      cardEntryConfig,
+      this.onCardNonceRequestSuccess,
+      this.onCardEntryCancel,
+    );
+  }
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+  render() {
+    return (
+      <ScrollView>
+        <View style={styles.sectionContainer}>
+          <Text style={styles.sectionTitle}>Square Up Payment</Text>
+          <Button title="Start Card Entry" />
         </View>
       </ScrollView>
-    </SafeAreaView>
-  );
-};
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+    backgroundColor: 'pink',
   },
   sectionTitle: {
     fontSize: 24,
     fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+    textAlign: 'center',
   },
 });
-
-export default App;
