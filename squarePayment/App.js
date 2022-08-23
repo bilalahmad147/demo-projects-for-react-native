cba1fdc046e0af59fa94d613fd16b4a2ab6cdfb4
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2022-08-23 23:15:00 +0500

    updated changes

--- squarePayment/App.js
+++ squarePayment/App.js
@@ -1,12 +1,45 @@
 import React from 'react';
 import {ScrollView, StyleSheet, Text, View} from 'react-native';
+// import {AdManager, ImageView} from 'react-native-admob-native-ads';
+// import NativeAdView from 'react-native-admob-native-ads';
+
+// const AdView = () => {
+//   const nativeAdViewRef = useRef();
+
+//   React.useEffect(() => {
+//     nativeAdViewRef.current?.loadAd();
+//   }, []);
+
+//   return (
+//     <NativeAdView
+//       ref={nativeAdViewRef}
+//       adUnitID="ca-app-pub-3940256099942544/2934735716">
+//       <View>
+//         <ImageView
+//           style={{
+//             width: '100%',
+//             height: 250,
+//           }}
+//         />
+//       </View>
+//     </NativeAdView>
+//   );
+// };
 
 const App = () => {
+  // AdManager.setRequestConfiguration({
+  //   testDeviceIds: ['ca-app-pub-3940256099942544/2934735716'],
+  // });
+
   return (
     <ScrollView>
-      <Header />
       <View>
         <Text>Hello World</Text>
+        {/* <NativeAdView adUnitID="	ca-app-pub-3940256099942544/2934735716">
+          <View>
+            <AdView />
+          </View>
+        </NativeAdView> */}
       </View>
     </ScrollView>
   );
