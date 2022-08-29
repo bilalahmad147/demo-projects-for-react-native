b6af94ae37e4b49a9dae6959b3876cfe20306aa9
Bilal Ahmad <bilalahmad147500@gmail.com>
2022-08-28 23:37:09 -0700

    Update App.js

--- testingapp/App.js
+++ testingapp/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 const App = () => {
   return (
-    <ScrollView>
-      <View style={styles.mainView}>
-        <Text>hello world</Text>
-      </View>
-    </ScrollView>
+    <View style={styles.container}>
+      <Text style={styles.heading}>TODO LIST</Text>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  mainView: {
-    backgroundColor: 'pink'
+  container: {
+    flex: 1,
+    backgroundColor: '#5f669c',
+  },
+  heading: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: '600',
+    marginTop: 30,
+    textAlign: 'center'
   },
 });
 
