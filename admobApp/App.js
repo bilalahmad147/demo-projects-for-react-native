d6e133a7d0d640cd38959908edc8d6a46b3916cd
Bilal Ahmad <bilalahmad147500@gmail.com>
2022-08-29 02:20:46 -0700

    updated addmob id for testing

--- admobApp/App.js
+++ admobApp/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {BannerAd, BannerAdSize, TestIds} from '@react-native-firebase/admob';
 
 const App = () => {
   return (
     <View style={styles.container}>
       <Text>hello world</Text>
+      <BannerAd
+        unitId={TestIds.BANNER}
+        size={BannerAdSize.SMART_BANNER}
+        requestOptions={{
+          requestNonPersonalizedAdsOnly: true,
+        }}
+        onAdLoaded={() => {
+          console.log('Advert loaded');
+        }}
+        onAdFailedToLoad={error => {
+          console.error('Advert failed to load: ', error);
+        }}
+      />
     </View>
   );
 };
@@ -12,7 +26,7 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: 'pink',
   },
 });
 
