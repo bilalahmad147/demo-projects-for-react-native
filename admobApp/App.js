5ddff8d685ec0b77c81256cdb724b3271c7f98b4
Bilal Ahmad <bilalahmad147500@gmail.com>
2022-08-29 04:35:56 -0700

    installed react native google mobile ads

--- admobApp/App.js
+++ admobApp/App.js
@@ -1,24 +1,10 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
-import {BannerAd, BannerAdSize, TestIds} from '@react-native-firebase/admob';
 
 const App = () => {
   return (
     <View style={styles.container}>
       <Text>hello world</Text>
-      <BannerAd
-        unitId={TestIds.BANNER}
-        size={BannerAdSize.SMART_BANNER}
-        requestOptions={{
-          requestNonPersonalizedAdsOnly: true,
-        }}
-        onAdLoaded={() => {
-          console.log('Advert loaded');
-        }}
-        onAdFailedToLoad={error => {
-          console.error('Advert failed to load: ', error);
-        }}
-      />
     </View>
   );
 };
