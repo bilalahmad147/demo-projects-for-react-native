f1c69ea9e88ef8ed55ac71555a9bd86df48e486b
Bilal Ahmad <bilalahmad147500@gmail.com>
2022-09-01 00:33:43 -0700

    Update App.js

--- testingapp/App.js
+++ testingapp/App.js
@@ -2,6 +2,20 @@ import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
 const App = () => {
+  let names = ['bilal', 'zubair', 'talha', 'ali', 'bil'];
+  let shortListed = [];
+  let rejected = [];
+  for (let value of names) {
+    if (value.length > 4) {
+      shortListed.push(value);
+    } else {
+      rejected.push(value);
+    }
+  }
+
+  console.log(shortListed);
+  console.log(rejected);
+
   return (
     <View style={styles.container}>
       <Text style={styles.heading}>TODO LIST</Text>
@@ -19,7 +33,7 @@ const styles = StyleSheet.create({
     fontSize: 30,
     fontWeight: '600',
     marginTop: 30,
-    textAlign: 'center'
+    textAlign: 'center',
   },
 });
 
