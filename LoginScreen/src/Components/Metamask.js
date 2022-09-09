a5844ddfd71488e012da5a99eef12fa7f3fab35b
Bilal Ahmad <bilalahmad147500@gmail.com>
2022-09-09 06:48:52 -0700

    updated

---
+++ LoginScreen/src/Components/Metamask.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function Metamask() {
+  return (
+    <View>
+      <Text>Metamask</Text>
+    </View>
+  );
+}
+
+export default Metamask;
